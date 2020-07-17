495f9f25b44faec538caded28b7015c51a5c03c2
rshakked <68347340+rshakked@users.noreply.github.com>
2020-07-17 12:52:11 +0300

    class square - work on it

---
+++ square.js
@@ -0,0 +1,11 @@
+class square {
+    constructor(x,y,img,boolean) {
+      this.x = x;
+      this.y = y;
+      this.img = img;
+      this.boolean = boolean;
+    }
+    show() {
+        ctx.drawImage(this.img, this.x, this.y);
+    }
+  }
