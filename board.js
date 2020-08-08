389daac50e713e4bb409cc5ee80e29c5fb042be2
rshakked <68347340+rshakked@users.noreply.github.com>
2020-08-08 13:38:03 +0300

    Function that create 2d array  of bool of square

--- board.js
+++ board.js
@@ -2,4 +2,17 @@ class board {
     constructor(arraySquare) {
     this.arraySquare = arraySquare;
     }
+    createArrayOfBool()
+    { //יצירת מערך דו ממדי בוליאני של כל המשבצות 
+        var arrayOfBool = new Array(10);
+        for(var i = 0; i < 10; i++)
+        {
+            arrayOfBool[i] = new Array(10);
+            for(var j = 0; j < 10; j++ )
+            {   
+                 arrayOfBool[i][j] = this.arraySquare[i][j].this.boolean;  
+            }
+        } 
+        return arrayOfBool;
+    } 
 }
