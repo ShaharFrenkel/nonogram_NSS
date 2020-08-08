3b5c6b55be658f39b48ada2b673b3d8ee0864cb1
Shachar Frenkel <68347106+ShaharFrenkel@users.noreply.github.com>
2020-08-08 23:28:44 +0300

    marged assinments and fixed almost all errors

--- table.js
+++ table.js
@@ -14,22 +14,24 @@ var is_filled = true; //the bool value that will replace the square's bool value
 var inOpenningScreen = true; //bool that states if the player is currantly viewing the opening page where he can begin the game
 var first_board = new Board();
 
-//בניית הטבלה של המשחק
-for (var i = start_point_x; i < 10 * square_size + 1 + start_point_x; i = i + square_size) {
-    ctx.moveTo(i, start_point_y);
-    ctx.lineTo(i, square_size * 10 + start_point_y);
-    ctx.stroke();
-
+function drawTable(){
+    //בניית הטבלה של המשחק
+    for (var i = start_point_x; i < 10 * square_size + 1 + start_point_x; i = i + square_size) {
+        ctx.moveTo(i, start_point_y);
+        ctx.lineTo(i, square_size * 10 + start_point_y);
+        ctx.stroke();
+    
+    }
+    for (var j = start_point_y; j < square_size * 10 + 1 + start_point_y; j = j + square_size) {
+        ctx.moveTo(start_point_x, j);
+        ctx.lineTo(square_size * 10 + start_point_x, j);
+        ctx.stroke();
+    }
 }
-for (var j = start_point_y; j < square_size * 10 + 1 + start_point_y; j = j + square_size) {
-    ctx.moveTo(start_point_x, j);
-    ctx.lineTo(square_size * 10 + start_point_x, j);
-    ctx.stroke();
-}
-
-
+first_board.showBoard();
+drawTable();
 //מערך דו מימדי של המשחק
-var array_name = [
+var first_level = [
     [false, false, false, false, true, true, false, false, false, false],
     [false, true, true, true, true, true, true, true, true, false],
     [false, true, false, false, false, false, false, false, true, false],
@@ -41,103 +43,106 @@ var array_name = [
     [true, true, true, true, true, true, true, true, true, true],
     [true, true, true, true, true, true, true, true, true, true],
 ];
-//הוספת המספרים לטבלה לפי שלב כללי
-var counterx = 0;
-var numberx = 0;
-var countery1 = 0;
-var numbery1 = 0;
 
-for( j=9; j>-1; j--)
-{   
-    counterx = 0;
-    numberx = 0;
-    for( i=9; i>-1; i--)
-    {       
-        if(array_name[j][i])
-        {
-            if((i==0)&&(numberx<9))
+function writeSideNumbers(boolArray)
+{
+    //הוספת המספרים לטבלה לפי שלב כללי
+    var counterx = 0;
+    var numberx = 0;
+    var countery1 = 0;
+    var numbery1 = 0;
+
+    for( j=9; j>-1; j--)
+    {   
+        counterx = 0;
+        numberx = 0;
+        for( i=9; i>-1; i--)
+        {       
+            if(boolArray[j][i])
             {
                 numberx++;
-                counterx++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30 + j*50);
+                if((i==0)&&(numberx<10))
+                {
+                    counterx++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30 + j*50);
+                }
+                if((i==0)&&(numberx>9))
+                {
+                    counterx++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numberx, start_point_x - counterx*30-18, start_point_y+30 + j*50);
+                }
             }
-            if((i==0)&&(numberx>8))
+            else   
             {
-                numberx++;
-                counterx++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numberx, start_point_x - counterx*30-18, start_point_y+30 + j*50);
+                if((numberx!=0) && (numberx<10))
+                {
+                    counterx++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30+ j*50);
+                    numberx = 0;
+                }
+                //תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
+                // אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
+                if((numberx>9)&&(numberx!=0))
+                {
+                    counterx++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numberx, start_point_x - counterx*30-30, start_point_y+30+ j*50);
+                    numberx = 0;
+                }
             }
-            numberx++;
         }
-        else   
+    }
+
+    for(j=9; j>-1; j--)
+    {
+        countery1 = 0;
+        numbery1 = 0;
+        for(i=9; i>-1; i--)
         {
-            if((numberx!=0) && (numberx<10))
+            if(boolArray[i][j])
             {
-                counterx++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30+ j*50);
-                numberx = 0;
+                numbery1++;
+                if((i==0)&&(numbery1<10))
+                {
+                    countery1++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numbery1, start_point_x+15 + j*50,  start_point_y -countery1*30+15 );
+                }
+                if((i==0)&&(numbery1>9))
+                {
+                    countery1++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numbery1, start_point_x+7 + j*50,  start_point_y -countery1*30+15 );
+                }
+                
             }
-            //תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
-            // אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
-            if((numberx>9)&&(numberx!=0))
-            {
-                counterx++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numberx, start_point_x - counterx*30-30, start_point_y+30+ j*50);
-                numberx = 0;
+            else
+                {
+                if((numbery1!=0)&&(numbery1<10))
+                {
+                    countery1++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numbery1, start_point_x+15 + j*50, start_point_y -countery1*30 +15);
+                    numbery1 = 0;
+                }
+                //תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
+                // אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
+
+                if((numbery1!=0)&&(numbery1>9)){
+                    countery1++;
+                    ctx.font = "30px Arial";
+                    ctx.fillText(numbery1, start_point_x+7 + j*50, start_point_y -countery1*30+15);
+                    numbery1 = 0;
+                }
+
             }
         }
     }
 }
-
-for(j=9; j>-1; j--)
-{
-    countery1 = 0;
-    numbery1 = 0;
-    for(i=9; i>-1; i--)
-    {
-        if(array_name[i][j])
-        {
-            if((i==0)&&(numbery1<9))
-            {
-                numbery1++;
-                countery1++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numbery1, start_point_x+15 + j*50,  start_point_y -countery1*30+15 );
-            }
-            if((i==0)&&(numbery1>8))
-            {
-                numbery1++;
-                countery1++;
-                ctx.font = "30px Arial";
-                ctx.fillText(numbery1, start_point_x+7 + j*50,  start_point_y -countery1*30+15 );
-            }
-            numbery1++;
-        }
-        else
-        {
-            if((numbery1!=0)&&(numbery1<10)){
-countery1++;
-ctx.font = "30px Arial";
-ctx.fillText(numbery1, start_point_x+15 + j*50, start_point_y -countery1*30 +15);
-numbery1 = 0;
-}
-//תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
-// אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
-
-if((numbery1!=0)&&(numbery1>9)){
-countery1++;
-ctx.font = "30px Arial";
-ctx.fillText(numbery1, start_point_x+7 + j*50, start_point_y -countery1*30+15);
-numbery1 = 0;
-}
-
-}
-}
-}
+writeSideNumbers(first_level);
 
 //display of a x/rect button
 function fill_button(is_rect_pressed){
@@ -201,46 +206,48 @@ function clickEvent(event) {
     }
     if(x >= 650 && x <= 780 && y >= 20 && y <= 120 )
     {
-        boardGame1.cleanBoard();
-        //boardGame1.showBoard();
+        first_board.cleanBoard();
         isClick = true;
     }
     
     //check if player clicked on a square, if so change its img and bool value acording to the button pressed in the fill/x option bottom of the screen
-    var found_clicked_square = false;
     var clicked_on_the_board = x>start_point_x && x<start_point_x+board_len_in_squares*square_size && y>start_point_y && y<start_point_y+board_len_in_squares*square_size;
+    var found_clicked_square = false;
     var line = 0;
     var coulmn = 0;
-    //an array for example to check my code, will later on be replaced by the array of the board
-    const temp_array = [
-        [new Square(start_point_x, start_point_y, empty_img, false),new Square(start_point_x+square_size,start_point_y, empty_img, false)],
-        [new Square(start_point_x, start_point_y+square_size, empty_img, false),new Square(start_point_x+square_size,start_point_y+square_size, empty_img, false)]
-    ];
-    //alert("x="+x+" y="+y+" "+ temp_array[line][coulmn]);
+    //alert("x="+x+" y="+y+" "+ first_board.arraySquares[line][coulmn].x);
 
     if(clicked_on_the_board){
-        while(!found_clicked_square){
+        while(found_clicked_square==false){
             //in this ine the console shows an error but the action
-            if(x>temp_array[line][coulmn].x && x<temp_array[line][coulmn].x+square_size && y>temp_array[line][coulmn].y && y<temp_array[line][coulmn].y+square_size){
-                temp_array[line][coulmn].img = fill_img;
-                temp_array[line][coulmn].boolean = is_filled;
-                temp_array[line][coulmn].show();
-                found_clicked_squre = true;
+            found_clicked_square = true;
+            coulmn++;
+            alert(coulmn);
+            /*if (x>first_board.arraySquares[line][coulmn].x && x<first_board.arraySquares[line][coulmn].x+square_size && y>first_board.arraySquares[line][coulmn].y && y<first_board.arraySquares[line][coulmn].y+square_size){
+                first_board.arraySquares[line][coulmn].img = fill_img;
+                first_board.arraySquares[line][coulmn].boolean = is_filled;
                 isClick = true;
+                found_clicked_squre = true;
             }
-            if(coulmn<temp_array[0].length-1){
+            if(coulmn<first_board.arraySquares[0].length-1){
                 coulmn++;
             }
             else{
                 coulmn = 0;
                 line++;
-            }
+            }*/
         }
     }
-    if( !isClick )
+
+    if(!isClick)
     {
         //אם המשחק לחץ בעכבר על מקום שלא אמור ללחוץ כותב הודעה לידע אותו
         alert("לחצתם על מקום עם העכבר שהוא לא קשור למשחק ");
+    }
+    else{
+        ctx.clearRect(start_point_x,start_point_y, 500, 500);
+        first_board.showBoard();
+        drawTable();
     }
 }
 function keyDownHandler(event)
