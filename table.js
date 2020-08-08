6fc05e67d7b9b0573981ec0646799e13ac342ae9
rshakked <68347340+rshakked@users.noreply.github.com>
2020-08-08 21:45:20 +0300

    tab

--- table.js
+++ table.js
@@ -6,9 +6,9 @@ const square_size = 50;
 const start_point_x = 150;
 const start_point_y = 150;
 var board_len_in_squares = 10;
-var rect_img = document.getElementById("rect_img");
-var x_img = document.getElementById("x_img");
-var empty_img = document.getElementById("empty_img");
+const rect_img = document.getElementById("rect_img");
+const x_img = document.getElementById("x_img");
+const empty_img = document.getElementById("empty_img");
 var fill_img = rect_img; //the img that will replace the empty image of a square objefct when the player clicks on the square
 var is_filled = true; //the bool value that will replace the square's bool value when the player clicks the square
 var inOpenningScreen = true; //bool that states if the player is currantly viewing the opening page where he can begin the game
@@ -40,9 +40,6 @@ var array_name = [
     [false, true, false, false, false, false, false, false, true, false],
     [true, true, true, true, true, true, true, true, true, true],
     [true, true, true, true, true, true, true, true, true, true],
-    
-    
-    
 ];
 //הוספת המספרים לטבלה לפי שלב כללי
 var counterx = 0;
@@ -50,73 +47,79 @@ var numberx = 0;
 var countery1 = 0;
 var numbery1 = 0;
 
-for( j=9; j>-1; j--){
-counterx = 0;
-numberx = 0;
-for( i=9; i>-1; i--){
-if(array_name[j][i]){
-if((i==0)&&(numberx<9)){
-numberx++;
-counterx++;
-ctx.font = "30px Arial";
-ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30 + j*50);
+for( j=9; j>-1; j--)
+{   
+    counterx = 0;
+    numberx = 0;
+    for( i=9; i>-1; i--)
+    {       
+        if(array_name[j][i])
+        {
+            if((i==0)&&(numberx<9))
+            {
+                numberx++;
+                counterx++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30 + j*50);
+            }
+            if((i==0)&&(numberx>8))
+            {
+                numberx++;
+                counterx++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numberx, start_point_x - counterx*30-18, start_point_y+30 + j*50);
+            }
+            numberx++;
+        }
+        else   
+        {
+            if((numberx!=0) && (numberx<10))
+            {
+                counterx++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30+ j*50);
+                numberx = 0;
+            }
+            //תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
+            // אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
+            if((numberx>9)&&(numberx!=0))
+            {
+                counterx++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numberx, start_point_x - counterx*30-30, start_point_y+30+ j*50);
+                numberx = 0;
+            }
+        }
+    }
 }
 
-if((i==0)&&(numberx>8)){
-numberx++;
-counterx++;
-ctx.font = "30px Arial";
-ctx.fillText(numberx, start_point_x - counterx*30-18, start_point_y+30 + j*50);
-
-}
-
-numberx++;
-}
-else{
-if((numberx!=0) && (numberx<10)){
-counterx++;
-ctx.font = "30px Arial";
-ctx.fillText(numberx, start_point_x - counterx*30, start_point_y+30+ j*50);
-numberx = 0;
-
-}
-//תנאי זה לא נחוץ במקרה שלנו באלס כיוון שאין מספר הגדול מ9 שלא נכנס באיף הראשון 
-// אך כדי לאפשר לעשות משחק עם לוח גדול יותר זה כן נחוץ פה
-if((numberx>9)&&(numberx!=0)){
-counterx++;
-ctx.font = "30px Arial";
-ctx.fillText(numberx, start_point_x - counterx*30-30, start_point_y+30+ j*50);
-numberx = 0;
-
-}
-
-}
-}
-}
-
-for(j=9; j>-1; j--){
-countery1 = 0;
-numbery1 = 0;
-for(i=9; i>-1; i--){
-if(array_name[i][j]){
-if((i==0)&&(numbery1<9)){
-numbery1++;
-countery1++;
-ctx.font = "30px Arial";
-ctx.fillText(numbery1, start_point_x+15 + j*50,  start_point_y -countery1*30+15 );
-}
-if((i==0)&&(numbery1>8)){
-numbery1++;
-countery1++;
-ctx.font = "30px Arial";
-ctx.fillText(numbery1, start_point_x+7 + j*50,  start_point_y -countery1*30+15 );
-}
-
-
-numbery1++;
-}
-else{
-if((numbery1!=0)&&(numbery1<10)){
+for(j=9; j>-1; j--)
+{
+    countery1 = 0;
+    numbery1 = 0;
+    for(i=9; i>-1; i--)
+    {
+        if(array_name[i][j])
+        {
+            if((i==0)&&(numbery1<9))
+            {
+                numbery1++;
+                countery1++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numbery1, start_point_x+15 + j*50,  start_point_y -countery1*30+15 );
+            }
+            if((i==0)&&(numbery1>8))
+            {
+                numbery1++;
+                countery1++;
+                ctx.font = "30px Arial";
+                ctx.fillText(numbery1, start_point_x+7 + j*50,  start_point_y -countery1*30+15 );
+            }
+            numbery1++;
+        }
+        else
+        {
+            if((numbery1!=0)&&(numbery1<10)){
 countery1++;
 ctx.font = "30px Arial";
 ctx.fillText(numbery1, start_point_x+15 + j*50, start_point_y -countery1*30 +15);
